f517313551f2ccb5b25233e8c8ea8980dec4f0a3
Askariois <askar_1996@list.ru>
2021-04-04 21:24:57 +0600

    edit

--- client/config/root.js
+++ client/config/root.js
@@ -86,7 +86,8 @@ const RootComponent = (props) => {
           <Switch>
             <Route exact path="/" component={() => <DummyView />} />
             <Route exact path="/dashboard" component={() => <Home />} />
-            <Route exact path="/dashboard/*" component={() => <Home />} />
+            <Route exact path="/dashboard/main" component={() => <Home />} />
+            <Route exact path="/dashboard/profile/:username" component={() => <Home />} />
             <PrivateRouteConnected exact path="/hidden-route" component={() => <DummyView />} />
             <Route component={() => <NotFound />} />
           </Switch>
